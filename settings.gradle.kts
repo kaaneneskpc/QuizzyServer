e9faf14a76418c1cce17451fc7ec3750b778721c
kkapici <kaaneneskpc1@gmail.com>
2025-08-13 22:36:50 +0300

    Create Quizzy Ktor Server

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "quizzy"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,13 @@
+package com.kaaneneskpc
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureFrameworks()
+    configureSerialization()
+    configureRouting()
+}
